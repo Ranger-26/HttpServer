4de2c04c876d90c88a1c498dad0feec56eda4d2c
smohan67 <siddhu.mohan6@gmail.com>
2022-08-28 16:31:49 -0500

    add in extensions

--- WebServer/Extensions/HttpRequestParsingExtensions.cs
+++ WebServer/Extensions/HttpRequestParsingExtensions.cs
@@ -1,4 +1,7 @@
-﻿using WebServer.Structs;
+﻿using System;
+using WebServer.Attributes;
+using WebServer.Enums;
+using WebServer.Structs;
 
 namespace WebServer.Extensions
 {
@@ -8,5 +11,37 @@ namespace WebServer.Extensions
         {
             return new HttpRequestInfo();
         }
+
+        public static HttpRequestLine GetHttpRequestLine(this string str)
+        {
+            string[] split = str.Split(' ');
+            foreach (var s in split)
+            {
+                Console.WriteLine(s);
+            }
+            HttpMethodType method;
+            if (!Enum.TryParse(split[0], true, out method))
+            {
+                Console.WriteLine($"Invalid request received: {split[0]}");
+            }
+
+            return new HttpRequestLine()
+            {
+                HttpMethodType = method,
+                Endpoint = split[1]
+            };
+        }
     }
 }
+/*
+GET /fff HTTP/1.1
+user-agent: Thunder Client (https://www.thunderclient.com)
+accept: /
+content-type: application/json
+content-length: 12
+accept-encoding: gzip, deflate, br
+Host: 127.0.0.1:13000
+Connection: close
+
+Testing body
+*/
