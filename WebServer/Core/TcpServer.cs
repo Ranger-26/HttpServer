335c4944c90bd9e09973896558395a54c688ea5a
smohan67 <siddhu.mohan6@gmail.com>
2022-08-28 03:03:52 -0500

    that took way too long

--- WebServer/Core/TcpServer.cs
+++ WebServer/Core/TcpServer.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading;
 
 namespace WebServer.Core
 {
@@ -18,7 +21,50 @@ namespace WebServer.Core
         public void StartServer()
         {
             _tcpListener.Start();
-            
+            while (true)
+            {
+                Console.Write("Waiting for a connection... ");
+
+                // Perform a blocking call to accept requests.
+                // You could also use server.AcceptSocket() here.
+                TcpClient client = _tcpListener.AcceptTcpClient();
+                Console.WriteLine("Connected!");
+                HandleTcpConnection(client);
+
+            }
+        }
+
+
+        void HandleTcpConnection(TcpClient client)
+        {
+            var thread = new Thread(() =>
+            {
+                NetworkStream stream = client.GetStream();
+
+                int i = stream.Read(_incomingStream, 0, _incomingStream.Length);
+
+
+
+                string data = System.Text.Encoding.ASCII.GetString(_incomingStream, 0, i);
+                Console.WriteLine("Received: {0}", data);
+
+
+                var result = "<h1>Hello, world!</h1>";
+                var sendBuf = Encoding.UTF8.GetBytes(
+                    "HTTP/1.0 200 OK" + Environment.NewLine
+                                      + "Content-Length: " + result.Length + Environment.NewLine
+                                      + "Content-Type: " + "text/plain" + Environment.NewLine
+                                      + Environment.NewLine
+                                      + result
+                                      + Environment.NewLine + Environment.NewLine);
+                stream.Write(sendBuf, 0, sendBuf.Length);
+
+
+
+                Console.WriteLine($"Buffer Length: {_incomingStream.Length}");
+                client.Close();
+            });
+            thread.Start();
         }
     }
 }
--- WebServer/Program.cs
+++ WebServer/Program.cs
@@ -6,7 +6,7 @@ namespace WebServer
     {
         public static void Main(string[] args)
         {
-            
+            new TcpServer(13000, "127.0.0.1").StartServer();
         }
     }
 }
