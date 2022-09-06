ce90c1e4a17e1d55557ac51e20e898b6c96c48a5
smohan67 <siddhu.mohan6@gmail.com>
2022-09-06 18:52:54 -0500

    it works

--- WebServer/Program.cs
+++ WebServer/Program.cs
@@ -12,7 +12,7 @@ namespace WebServer
         {
             var request = "GET / HTTP/1.1\r\nuser-agent: Thunder Client (https://www.thunderclient.com)\r\naccept: /\r\ncontent-type: application/json\r\ncontent-length: 12\r\naccept-encoding: gzip, deflate, br\r\nHost: 127.0.0.1:13000\r\nConnection: close\r\n\r\nTesting body";
 
-            new TcpServer(7777, "127.0.0.1").StartServer();
+            new TcpServer(7777, "0.0.0.0").StartServer();
         }
     }
 
