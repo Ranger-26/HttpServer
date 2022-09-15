7036bad27688a111c47c5552e9613b6f79543e07
Siddhu M <61168189+RedRangerr@users.noreply.github.com>
2022-09-15 14:59:10 -0500

    start on httpresponse stuff

---
+++ WebServer/Structs/HttpResponseInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebServer.Structs
+{
+    public struct HttpResponseInfo
+    {
+        public int HttpCode;
+
+        public Dictionary<string, string> Headers;
+
+        public string Body;
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
